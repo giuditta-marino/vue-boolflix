356dfc1648a41475832a066d6bdc3b167040c370
Giuditta <giudittamarino89@gmail.com>
2021-04-13 11:16:04 +0200

    perfezionata milestone 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,15 @@ var app = new Vue({
   el: '#root',
   data: {
     searchInput: '',
-    searchResult: []
+    searchResult: [],
+    api_key: '59d9354a6ba6f6c0a81844a496f08846',
+    uri: 'https://api.themoviedb.org/3',
+    language: 'it'
   },
 
   methods: {
     search: function(){
-      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=59d9354a6ba6f6c0a81844a496f08846&query=${this.searchInput}&language=it`)
+      axios.get(`${this.uri}/search/movie?api_key=${this.api_key}&query=${this.searchInput}&language=${this.language}`)
       .then((response) => {
         console.log(response.data.results);
         console.log(this.searchResult)
