4a6eee27455fdbe6bff2c5c6eb2e4996af625f71
Giuditta <giudittamarino89@gmail.com>
2021-04-14 15:55:11 +0200

    esercizio terminato inizio bonus milestone 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,7 @@ var app = new Vue({
     uri: 'https://api.themoviedb.org/3',
     language: 'it',
     availableFlag: ['en', 'it', 'es', 'fr', 'pt', 'ru', 'de', 'zh          '],
+    cast: []
   },
 
   mounted(){
@@ -24,18 +25,13 @@ var app = new Vue({
 
         this.searchResult = [...this.searchResult, ...response.data.results];
 
-        console.log(this.searchResult);
-
         axios.get(`${this.uri}/search/tv?api_key=${this.api_key}&query=${this.searchInput}&language=${this.language}`)
         .then((response) => {
-          console.log(response.data.results);
 
           this.searchResult = [...this.searchResult, ...response.data.results];
 
-          console.log(this.searchResult);
         });
       });
-
 
     },
 
@@ -59,6 +55,27 @@ var app = new Vue({
 
       n = Math.floor(n / 2);
       return n;
+
+    },
+
+    showCast: function(i){
+      // al click, devo prendere il film specifico
+      // e prenderne l'id
+      // ${this.uri}/search/
+
+      console.log(this.searchResult[i].id);
+      const id = this.searchResult[i].id;
+
+      axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=59d9354a6ba6f6c0a81844a496f08846`).
+      then((response) =>{
+        console.log(response.data.cast.slice(0,5));
+        const actors = response.data.cast.slice(0,5);
+        actors.forEach((item, i) => {
+          this.cast.push(item.name);
+        });
+
+        console.log(this.cast);
+      });
 
     }
 
