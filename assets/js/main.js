1fc376ba0b708baa2e58ce843af2ac7782929bd2
giuditta-marino <77673857+giuditta-marino@users.noreply.github.com>
2021-08-23 18:46:03 +0200

    functions finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,35 +38,41 @@ var app = new Vue({
       this.popularAll = [...this.popularMovies, ...this.popularSeries];
     });
 
-    // Prendo i generi dei film
+    // Prendo i generi dei film e li pusho in genres
     axios.get(`${this.uri}/genre/movie/list?api_key=${this.api_key}`)
     .then((response) => {
-      let movieGenres = [...response.data.genres];
-      movieGenres.forEach((item) => {
-        this.genres.push(item.name);
-      });
+      this.genres = [...response.data.genres];
     });
 
-    // Prendo i generi delle serie
+    // Prendo i generi delle serie e li pusho in genres se non sono contenuti già usando due variabili di appoggio
     axios.get(`${this.uri}/genre/tv/list?api_key=${this.api_key}`)
     .then((response) => {
-      let serieGenres = [...response.data.genres];
-      serieGenres.forEach((item) => {
-        if (!this.genres.includes(item.name)) {
-          this.genres.push(item.name);
-        }
-      });
+        let movieGenres =[];
+        this.genres.forEach((item, i) => {
+          movieGenres.push(item.name)
+        });
+        let serieGenres = [... response.data.genres];
+        serieGenres.forEach((item, i) => {
+          if (!movieGenres.includes(item.name)) {
+            this.genres.push(item);
+          }
+        });
+        // ordino alfabeticamente i nomi dei generi
+        this.genres.sort(function(a, b){
+          if(a.name < b.name) {
+            return -1;
+          }
+          if(a.name > b.name) {
+            return 1;
+          }
+          return 0;
+        });
     });
-  },
-
-  computed: {
 
   },
 
   methods: {
-    showGenre: function(){
-      console.log(this.selectedGenre);
-    },
+    // funzione di ricerca tramite chiamate axios
     search: function(){
       this.searchResult = [];
       this.isHidden = false;
@@ -80,7 +86,6 @@ var app = new Vue({
         // salvo in searchResult i risultati dei film per sommarli a quelli delle serie
         this.searchResult = [...this.searchResult, ...response.data.results];
 
-
         axios.get(`${this.uri}/search/tv?api_key=${this.api_key}&query=${this.searchInput}&language=${this.language}`)
         .then((response) => {
           // salvo in una variabile i risultati delle serie
@@ -92,31 +97,55 @@ var app = new Vue({
     },
 
     filterCategory: function() {
-      // filtro per categoria sia i risultati della ricerca che i popolari in firstPage
+      // filtro per categoria e per genere sia i risultati della ricerca che i popolari in firstPage
       if (this.category == 'Titoli') {
         if (this.firstPage) {
+          if (this.selectedGenre != ''){
+            let filtered = this.popularAll.filter((item)=>
+            item.genre_ids.includes(this.selectedGenre));
+            return filtered;
+          }
           return this.popularAll;
         } else {
-          if (this.selectedGenre != '') {
-            this.searchResult.filter((item) => item.genre_ids.includes(this.selectedGenre));
+            if (this.selectedGenre != '') {
+              let filtered = this.searchResult.filter((item) =>
+              item.genre_ids.includes(this.selectedGenre));
+              return filtered;
+            }
+            return this.searchResult;
           }
-          return this.searchResult;
-        }
       } else if (this.category == 'Film') {
         if (this.firstPage) {
+          if (this.selectedGenre != '') {
+            let filtered = this.popularMovies.filter((item)=> item.genre_ids.includes(this.selectedGenre));
+            return filtered;
+          }
           return this.popularMovies;
         } else {
+          if (this.selectedGenre != '') {
+            let filtered = this.movieResult.filter((item)=> item.genre_ids.includes(this.selectedGenre));
+            return filtered;
+          }
           return this.movieResult;
         }
       } else {
         if (this.firstPage) {
+          if (this.selectedGenre != '') {
+            let filtered = this.popularSeries.filter((item)=> item.genre_ids.includes(this.selectedGenre));
+            return filtered;
+          }
           return this.popularSeries;
         } else {
+          if (this.selectedGenre != '') {
+            let filtered = this.seriesResult.filter((item)=> item.genre_ids.includes(this.selectedGenre));
+            return filtered;
+          }
           return this.seriesResult;
         }
       }
     },
 
+    // prendo i titoli dei film e delle serie
     getTitle: function(obj){
       if (obj.title) {
         return obj.title
@@ -125,6 +154,7 @@ var app = new Vue({
       }
     },
 
+    // prendo i titoli originali dei film e delle serie
     getOriginalTitle: function(obj){
       if (obj.original_title) {
         return obj.original_title
@@ -133,17 +163,15 @@ var app = new Vue({
       }
     },
 
+    // trasformo il voto per esprimerlo in quinti
     voteTransform: function(n){
-
       n = Math.ceil(n / 2);
       return n;
-
     },
 
+    // al click, devo prendere il film specifico
+    // e prenderne l'id per poter, tramite chiamata axios, ottenere il cast
     showCast: function(i){
-      // al click, devo prendere il film specifico
-      // e prenderne l'id
-      // ${this.uri}/search/
       this.cast =[];
 
       const id = this.searchResult[i].id;
@@ -157,18 +185,36 @@ var app = new Vue({
       });
     },
 
+    // funzione per mostrare cast dei film popolari
+    showCastPop: function(i){
+      this.cast =[];
+
+      const id = this.popularAll[i].id;
+
+      axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=59d9354a6ba6f6c0a81844a496f08846`).
+      then((response) =>{
+        const actors = response.data.cast.slice(0,5);
+        actors.forEach((item, i) => {
+          this.cast.push(item.name);
+        });
+      });
+    },
+
+    // al click sulla categoria, prendo il testo dell'a e lo assegno a this.category
     selectCategory: function(e){
-      // al click sulla categoria, prendo il testo dell'a e lo assegno a this.category
       this.category = e.target.innerHTML;
       this.selectedGenre = '';
     },
 
+    // al click sul logo, torno ai film popolari
     homePage: function () {
-     // this.firstPage = true;
+     this.firstPage = true;
+     this.searchInput = '';
      this.category = 'Titoli';
      this.selectedGenre = '';
    },
 
+   // titolo dinamico che indica il contenuto dei risultati
    resultsTitle: function () {
       if (this.firstPage) {
         return `Popolari su Boolflix - ${this.category}`;
